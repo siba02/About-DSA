7fc51fa081030d943f4441cde19518085725d2ea
sibu02 <sibaprasad.tofan@gmail.com>
2024-07-06 00:14:36 +0530

    modify in code

--- largestelement.java
+++ largestelement.java
@@ -1,7 +1,7 @@
 class largestelement {
     public static int findlargestelement(int[]arr){
-        int largest=arr[0];
-        for(int i=1;i<arr.length;i++){
+        int largest=0;
+        for(int i=0;i<arr.length;i++){
             if(arr[i]>largest){
                 largest=arr[i];
             }
@@ -10,7 +10,7 @@ class largestelement {
 }
     public static void main(String[] args){
         int[]array={50,42,38,10,29,44};
-        int maxelement = findlargestelement(array);
+        int maxelement =largestelement.findlargestelement(array);
     System.out.println("largestelement:"+maxelement);
     }
 }
